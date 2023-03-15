6455c54841559562553265b9f7b21b7e18ca3e5f
Raphael Lima <phaelslima@gmail.com>
2023-03-15 18:58:28 -0300

    feat: add group list to groups screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,31 @@
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+
+import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { ListEmpty } from '@components/ListEmpty'
 
 import { Container } from './styles'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(_, index) => String(index)}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal Cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   )
 }
