5a4652e81d3ac8cb43d3bf0d5caa83591768b256
Raphael Lima <phaelslima@gmail.com>
2023-03-17 19:15:26 -0300

    feat: add players screen

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import {
 
 import { Loading } from '@components/Loading'
 
-import { NewGroup } from '@screens/NewGroup'
+import { Players } from '@screens/Players'
 
 import { defaultTheme } from '@theme/default'
 
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   )
 }
---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,25 @@
+import { ButtonIcon } from '@components/ButtonIcon'
+import { Header } from '@components/Header'
+import { Highlight } from '@components/Highlight'
+import { Input } from '@components/Input'
+
+import { Container, Form } from './styles'
+
+export function Players() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight
+        title="Nome da turma"
+        subtitle="adicione a galera e separe os times"
+      />
+
+      <Form>
+        <Input placeholder="Nome do participante" autoCorrect={false} />
+
+        <ButtonIcon icon="add" />
+      </Form>
+    </Container>
+  )
+}
