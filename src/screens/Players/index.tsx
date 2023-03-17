6f833f2b35bf7add46ca2031af96ab9dd3a5d5e9
Raphael Lima <phaelslima@gmail.com>
2023-03-17 19:58:02 -0300

    feat: add playerCard list to players screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import { FlatList } from 'react-native'
 
+import { Button } from '@components/Button'
 import { ButtonIcon } from '@components/ButtonIcon'
 import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
+import { ListEmpty } from '@components/ListEmpty'
+import { PlayerCard } from '@components/PlayerCard'
 
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles'
 
@@ -44,6 +47,24 @@ export function Players() {
 
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(_, index) => String(index)}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+
+      <Button title="Remover turma" type="SECONDARY" />
     </Container>
   )
 }
