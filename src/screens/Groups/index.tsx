b38ddb6d17427dee594e63491875bdb82a759252
Raphael Lima <phaelslima@gmail.com>
2023-03-19 23:57:43 -0300

    feat: add method to get all groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { FlatList } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+
+import { groupsGetAll } from '@storage/group/groupsGetAll'
 
 import { Button } from '@components/Button'
 import { GroupCard } from '@components/GroupCard'
@@ -18,6 +20,21 @@ export function Groups() {
   function handleNewGroup() {
     navigation.navigate('new')
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll()
+      setGroups(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups()
+    }, [])
+  )
 
   return (
     <Container>
