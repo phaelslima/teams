22a789f20986f19b5dfc3512429a68fe51b1d0de
Raphael Lima <phaelslima@gmail.com>
2023-03-19 23:58:23 -0300

    feat: add method to create group

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -6,6 +6,8 @@ import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
 
+import { groupCreate } from '@storage/group/groupCreate'
+
 import { Container, Content, Icon } from './styles'
 
 export function NewGroup() {
@@ -13,8 +15,13 @@ export function NewGroup() {
 
   const navigation = useNavigation()
 
-  function handleNew() {
-    navigation.navigate('players', { group })
+  async function handleNew() {
+    try {
+      await groupCreate(group)
+      navigation.navigate('players', { group })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
